1f89c6b0120e62f9cf636105966d81bd4fc638b7
kuehnmic <kuehnmic@fde32c5b-6e3e-0410-ac5f-c52931e6f8ac>
2007-11-13 10:29:26 +0000

    ADD added template for Database Access

---
+++ develop/de/dini/oanetzwerk/dbAccess.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+package de.dini.oanetzwerk;
+
+
+/**
+ * @author Michael Kühn
+ *
+ */
+
+public class dbAccess {
+
+	/**
+	 * @param args
+	 */
+	
+	public static void main (String [ ] args) {
+		
+		System.out.println ("DB-Test");
+	}
+}
