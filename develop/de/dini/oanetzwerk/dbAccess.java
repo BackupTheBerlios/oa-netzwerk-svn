809ef2bd9d49682d3b08647aea3f4d24314768d1
kuehnmic <kuehnmic@fde32c5b-6e3e-0410-ac5f-c52931e6f8ac>
2007-11-13 15:38:33 +0000

    UPDATE added DB-Connection, which is still untested due to a lack of a Sybase DBMS Instance.

--- develop/de/dini/oanetzwerk/dbAccess.java
+++ develop/de/dini/oanetzwerk/dbAccess.java
@@ -4,6 +4,19 @@
 
 package de.dini.oanetzwerk;
 
+import java.sql.BatchUpdateException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.naming.Reference;
+import javax.naming.StringRefAddr;
+import javax.sql.DataSource;
+
+import junit.framework.Assert;
 
 /**
  * @author Michael Kühn
@@ -16,8 +29,280 @@ public class dbAccess {
 	 * @param args
 	 */
 	
+	static Connection conn = null;
+	static Statement stmt = null;
+	
 	public static void main (String [ ] args) {
 		
 		System.out.println ("DB-Test");
+		
+		if (args [0].equals ("--createDB")) {
+			
+			createDB ( );
+		}
+	}
+
+	/**
+	 * 
+	 */
+	
+	private static void createDB ( ) {
+				
+		System.setProperty (Context.INITIAL_CONTEXT_FACTORY, "org.apache.naming.java.javaURLContextFactory");
+		System.setProperty (Context.PROVIDER_URL, "file:///tmp");
+		DataSource ds = null;
+		InitialContext ic = null;
+		InitialContext ic2 = null;
+		
+		@SuppressWarnings("unused")
+		int [ ] updateCounts = null;
+		
+		try {
+			
+			ic = new InitialContext ( );
+			
+			Reference cpdsRef = new Reference ("org.apache.commons.dbcp.cpdsadapter.DriverAdapterCPDS",
+					"org.apache.commons.dbcp.cpdsadapter.DriverAdapterCPDS", null);
+			cpdsRef.add (new StringRefAddr ("driver", "com.sybase.jdbc2.jdbc.SybDriver"));
+			cpdsRef.add (new StringRefAddr ("url", "jdbc:sybase:Tds:host:1234/database"));
+			cpdsRef.add (new StringRefAddr ("user", "foo"));
+			cpdsRef.add (new StringRefAddr ("password", "bar"));
+			ic.rebind ("oanetzwerk", cpdsRef);
+			
+			Reference ref = new Reference ("org.apache.commons.dbcp.datasources.PerUserPoolDataSource",
+					"org.apache.commons.dbcp.datasources.PerUserPoolDataSourceFactory", null);
+			ref.add (new StringRefAddr ("dataSourceName", "oanetzwerk"));
+			ref.add (new StringRefAddr ("defaultMaxActive", "100"));
+			ref.add (new StringRefAddr ("defaultMaxIdle", "30"));
+			ref.add (new StringRefAddr ("defaultMaxWait", "10000"));
+			ic.rebind ("oanetzwerk", ref);
+			
+			ic2 = new InitialContext ( );
+			
+			ds = (DataSource) ic2.lookup ("oanetzwerk");
+			Assert.assertNotNull (ds);
+			conn = ds.getConnection ( );
+			
+			conn.setAutoCommit (false);
+			stmt.addBatch ("CREATE TABLE dbo.AggregatorMetadata");
+			updateCounts = stmt.executeBatch ( );
+			conn.setAutoCommit (true);
+			
+/*			stmt.executeUpdate ("DROP TABLE dbo.AggregatorMetadata");
+			stmt.executeUpdate ("CREATE TABLE dbo.AggregatorMetadata (" +
+					"object_id INTEGER NOT NULL" +
+					", harvested TIMESTAMP NOT NULL" +
+					", PRIMARY KEY (object_id, harvested)" +
+					");");
+			
+			stmt.executeUpdate ("DROP TABLE dbo.Person");
+			stmt.executeUpdate ("CREATE TABLE dbo.Person (" +
+					"person_id INTEGER NOT NULL" +
+					", number INTEGER" +
+					", firstname VARCHAR(100)" +
+					", lastname CHAR" +
+					", title CHAR" +
+					", institution CHAR" +
+					", email CHAR" +
+					", PRIMARY KEY (person_id)" +
+					");");
+			
+			stmt.executeUpdate ("DROP TABLE dbo.Keywords");
+			stmt.executeUpdate ("CREATE TABLE dbo.Keywords (" +
+					"keyword_id INTEGER NOT NULL" +
+				    ", keyword VARCHAR(255)" +
+				    ", lang CHAR(3)" +
+				    ", PRIMARY KEY (keyword_id)" +
+					");");
+			
+			stmt.executeUpdate ("DROP TABLE dbo.DCC_Categories");
+			stmt.executeUpdate ("CREATE TABLE dbo.DCC_Categories (" +
+					"DCC_Categorie CHAR(4) NOT NULL" +
+				    ", name VARCHAR(255)" +
+				    ", PRIMARY KEY (DCC_Categorie)" +
+					");");
+			
+			stmt.executeUpdate ("DROP TABLE dbo.DNB_Categories");
+			stmt.executeUpdate ("CREATE TABLE dbo.DNB_Categories (" +
+				    "DNB_Categorie INTEGER NOT NULL" +
+				    ", name VARCHAR(255)" +
+				    ", PRIMARY KEY (DNB_Categorie)" +
+					");");
+			
+			stmt.executeUpdate ("DROP TABLE dbo.DINI_Set_Categories");
+			stmt.executeUpdate ("CREATE TABLE dbo.DINI_Set_Categories (" +
+				    "DINI_set_id INTEGER NOT NULL" +
+				    ", name VARCHAR(255) NOT NULL" +
+				    ", PRIMARY KEY (DINI_set_id)" +
+					");");
+			
+			stmt.executeUpdate ("DROP TABLE dbo.Other_Categories");
+			stmt.executeUpdate ("CREATE TABLE dbo.Other_Categories (" +
+				    "other_id INTEGER NOT NULL" +
+				    ", name VARCHAR(255) NOT NULL" +
+				    ", PRIMARY KEY (other_id)" +
+					");");
+
+			stmt.executeUpdate ("DROP TABLE dbo.Repositories");
+			stmt.executeUpdate ("CREATE TABLE dbo.Repositories (" +
+				    "repository_id INTEGER NOT NULL" +
+				    ", PRIMARY KEY (repository_id)" +
+					");");
+
+			stmt.executeUpdate ("DROP TABLE dbo.Objec");
+			stmt.executeUpdate ("CREATE TABLE dbo.Object (" +
+				    "object_id INTEGER NOT NULL" +
+				    ", repository_id INTEGER NOT NULL" +
+				    ", harvested TIMESTAMP NOT NULL" +
+				    ", repository_datestamp DATE" +
+				    ", repository_identifier VARCHAR(255) NOT NULL" +
+				    ", PRIMARY KEY (object_id)" +
+				    ", FK_Object_1 FOREIGN KEY (repository_id)" +
+				    	"REFERENCES dbo.Repositories (repository_id)" +
+				    ", FK_Object_2 FOREIGN KEY (object_id, harvested)" +
+				    	"REFERENCES dbo.AggregatorMetadata (object_id, harvested)" +
+				    ");");
+
+			stmt.executeUpdate ("DROP TABLE dbo.Titles");
+			stmt.executeUpdate ("CREATE TABLE dbo.Titles (" +
+				    "object_id INTEGER NOT NULL" +
+				    ", title CHAR NOT NULL" +
+				    ", number INTEGER NOT NULL" +
+				    ", PRIMARY KEY (object_id)" +
+				    ", FK_Titles_1 FOREIGN KEY (object_id)" +
+				    	"REFERENCES dbo.Object (object_id)" +
+				    ");");
+
+			stmt.executeUpdate ("DROP TABLE dbo.Object2Author");
+			stmt.executeUpdate ("CREATE TABLE dbo.Object2Author (" +
+				    "object_id INTEGER NOT NULL" +
+				    ", author_id INTEGER NOT NULL" +
+				    ", PRIMARY KEY (object_id, author_id)" +
+				    ", FK_Object2Author_2 FOREIGN KEY (object_id)" +
+				    	"REFERENCES dbo.Object (object_id)" +
+				    ", FK_Object2Author_3 FOREIGN KEY (author_id)" +
+				    	"REFERENCES dbo.Person (person_id)" +
+				    ");");
+
+			stmt.executeUpdate ("DROP TABLE dbo.Object2Contributor");
+			stmt.executeUpdate ("CREATE TABLE dbo.Object2Contributor (" +
+				    "object_id INTEGER NOT NULL" +
+				    ", contributor_id INTEGER NOT NULL" +
+				    ", PRIMARY KEY (object_id, contributor_id)" +
+				    ", FK_Object2Contributor_1 FOREIGN KEY (object_id)" +
+				    	"REFERENCES dbo.Object (object_id)" +
+				    ", FK_Object2Contributor_2 FOREIGN KEY (contributor_id)" +
+				    	"REFERENCES dbo.Person (person_id)" +
+				    ");");
+
+			stmt.executeUpdate ("DROP TABLE dbo.Object2Editor");
+			stmt.executeUpdate ("CREATE TABLE dbo.Object2Editor (" +
+				    "object_id INTEGER NOT NULL" +
+				    ", editor_id INTEGER NOT NULL" +
+				    ", PRIMARY KEY (object_id, editor_id)" +
+				    ", FK_Object2Editor_2 FOREIGN KEY (object_id)" +
+				    	"REFERENCES dbo.Object (object_id)" +
+				    ", FK_Object2Editor_1 FOREIGN KEY (editor_id)" +
+				    	"REFERENCES dbo.Person (person_id)" +
+					");");
+
+			stmt.executeUpdate ("DROP TABLE dbo.Object2Keywords");
+			stmt.executeUpdate ("CREATE TABLE dbo.Object2Keywords (" +
+				    "object_id INTEGER NOT NULL" +
+				    ", keyword_id INTEGER NOT NULL" +
+				    ", PRIMARY KEY (object_id, keyword_id)" +
+				    ", FK_Object2Keywords_1 FOREIGN KEY (object_id)" +
+				    	"REFERENCES dbo.Object (object_id)" +
+				    ", FK_Object2Keywords_2 FOREIGN KEY (keyword_id)" +
+				    	"REFERENCES dbo.Keywords (keyword_id)" +
+				    ");");
+
+			stmt.executeUpdate ("DROP TABLE dbo.DCC_Classification");
+			stmt.executeUpdate ("CREATE TABLE dbo.DCC_Classification (" +
+				    "object_id INTEGER NOT NULL" +
+				    ", DCC_Categorie CHAR(10) NOT NULL" +
+				    ", PRIMARY KEY (object_id, DCC_Categorie)" +
+				    ", FK_DCC_Classification_1 FOREIGN KEY (DCC_Categorie)" +
+				    	"REFERENCES dbo.DCC_Categories (DCC_Categorie)" +
+				    ", FK_DCC_Classification_2 FOREIGN KEY (object_id)" +
+				    	"REFERENCES dbo.Object (object_id)" +
+				    ");");
+
+			stmt.executeUpdate ("DROP TABLE dbo.DNB_Classification");
+			stmt.executeUpdate ("CREATE TABLE dbo.DNB_Classification (" +
+				    "object_id INTEGER NOT NULL" +
+				    ", DNB_Categorie INTEGER NOT NULL" +
+				    ", PRIMARY KEY (object_id, DNB_Categorie)" +
+				    ", FK_DNB_Classification_1 FOREIGN KEY (DNB_Categorie)" +
+				    	"REFERENCES dbo.DNB_Categories (DNB_Categorie)" +
+				    ", FK_DNB_Classification_2 FOREIGN KEY (object_id)" +
+				    	"REFERENCES dbo.Object (object_id)" +
+				    ");");
+
+			stmt.executeUpdate ("DROP TABLE dbo.DINI_Set_Classificatio");
+			stmt.executeUpdate ("CREATE TABLE dbo.DINI_Set_Classification (" +
+				    "object_id INTEGER NOT NULL" +
+				    ", DINI_set_id INTEGER NOT NULL" +
+				    ", PRIMARY KEY (object_id, DINI_set_id)" +
+				    ", FK_DINI_Set_Classification_1 FOREIGN KEY (DINI_set_id)" +
+				    	"REFERENCES dbo.DINI_Set_Categories (DINI_set_id)" +
+				    ", FK_DINI_Set_Classification_2 FOREIGN KEY (object_id)" +
+				    	"REFERENCES dbo.Object (object_id)" +
+				    ");");
+
+			stmt.executeUpdate ("DROP TABLE dbo.Other_Classification");
+			stmt.executeUpdate ("CREATE TABLE dbo.Other_Classification (" +
+				    "object_id INTEGER NOT NULL" +
+				    ", other_id INTEGER NOT NULL" +
+				    ", PRIMARY KEY (object_id, other_id)" +
+				    ", FK_Other_Classification_1 FOREIGN KEY (other_id)" +
+				    	"REFERENCES dbo.Other_Categories (other_id)" +
+				    ", FK_Other_Classification_2 FOREIGN KEY (object_id)" +
+				    	"REFERENCES dbo.Object (object_id)" +
+				    ");");
+
+			stmt.executeUpdate ("DROP TABLE dbo.RawData");
+			stmt.executeUpdate ("CREATE TABLE dbo.RawData (" +
+				    "object_id INTEGER NOT NULL" +
+				    ", collected TIMESTAMP NOT NULL" +
+				    ", data CHAR" +
+				    ", PRIMARY KEY (object_id, collected)" +
+				    ", FK_RawData_1 FOREIGN KEY (object_id)" +
+				    	"REFERENCES dbo.Object (object_id)" +
+				    ");");*/
+		
+		} catch (BatchUpdateException buex) {
+			
+			try {
+				
+				conn.rollback ( );
+				
+			} catch (SQLException ex) {
+				
+				ex.printStackTrace ( );
+			}
+			
+		} catch (SQLException ex) {
+			
+			ex.printStackTrace ( );
+			
+		} catch (NamingException ex) {
+			
+			ex.printStackTrace ( );
+			
+		} /*finally {
+			
+			if (conn != null) {
+				
+				try {
+					
+					conn.close ( );
+					
+				} catch (SQLException sqlex) {
+					
+					sqlex.printStackTrace ( );
+				}
+			}
+		}*/
 	}
 }
