0c14478bda39f0b2e4b4230eb60b7511e8a545af
kuehnmic <kuehnmic@fde32c5b-6e3e-0410-ac5f-c52931e6f8ac>
2007-11-20 10:16:42 +0000

    UPDATE changed sql-code so that it works with Sybase Adaptive Server Enterprise had to replace timestamp with datetime and remove foreign-keytable-names. blob does also not exist. It is called text or image as far as I know. so i chose text, due to the xml we will put in there

--- develop/db-schema/oan-db-model.sql
+++ develop/db-schema/oan-db-model.sql
@@ -1,6 +1,6 @@
 CREATE TABLE dbo.AggregatorMetadata (
        object_id INTEGER NOT NULL
-     , harvested TIMESTAMP NOT NULL
+     , harvested datetime NOT NULL
      , PRIMARY KEY (object_id)
 );
 
@@ -54,13 +54,13 @@ CREATE TABLE dbo.Repositories (
 CREATE TABLE dbo.Object (
        object_id INTEGER NOT NULL
      , repository_id INTEGER NOT NULL
-     , harvested TIMESTAMP NOT NULL
+     , harvested datetime NOT NULL
      , repository_datestamp DATE
      , repository_identifier VARCHAR(255) NOT NULL
      , PRIMARY KEY (object_id)
-     , FK_Object_1 FOREIGN KEY (repository_id)
+     , FOREIGN KEY (repository_id)
                   REFERENCES dbo.Repositories (repository_id)
-     , FK_Object_2 FOREIGN KEY (object_id)
+     , FOREIGN KEY (object_id)
                   REFERENCES dbo.AggregatorMetadata (object_id)
 );
 
@@ -70,7 +70,7 @@ CREATE TABLE dbo.Titles (
      , qualifier VARCHAR(50) NOT NULL
      , lang VARCHAR(3)
      , PRIMARY KEY (object_id, qualifier)
-     , FK_Titles_1 FOREIGN KEY (object_id)
+     , FOREIGN KEY (object_id)
                   REFERENCES dbo.Object (object_id)
 );
 
@@ -78,9 +78,9 @@ CREATE TABLE dbo.Object2Author (
        object_id INTEGER NOT NULL
      , author_id INTEGER NOT NULL
      , PRIMARY KEY (object_id, author_id)
-     , FK_Object2Author_2 FOREIGN KEY (object_id)
+     , FOREIGN KEY (object_id)
                   REFERENCES dbo.Object (object_id)
-     , FK_Object2Author_3 FOREIGN KEY (author_id)
+     , FOREIGN KEY (author_id)
                   REFERENCES dbo.Person (person_id)
 );
 
@@ -88,9 +88,9 @@ CREATE TABLE dbo.Object2Contributor (
        object_id INTEGER NOT NULL
      , contributor_id INTEGER NOT NULL
      , PRIMARY KEY (object_id, contributor_id)
-     , FK_Object2Contributor_1 FOREIGN KEY (object_id)
+     , FOREIGN KEY (object_id)
                   REFERENCES dbo.Object (object_id)
-     , FK_Object2Contributor_2 FOREIGN KEY (contributor_id)
+     , FOREIGN KEY (contributor_id)
                   REFERENCES dbo.Person (person_id)
 );
 
@@ -98,9 +98,9 @@ CREATE TABLE dbo.Object2Editor (
        object_id INTEGER NOT NULL
      , editor_id INTEGER NOT NULL
      , PRIMARY KEY (object_id, editor_id)
-     , FK_Object2Editor_2 FOREIGN KEY (object_id)
+     , FOREIGN KEY (object_id)
                   REFERENCES dbo.Object (object_id)
-     , FK_Object2Editor_1 FOREIGN KEY (editor_id)
+     , FOREIGN KEY (editor_id)
                   REFERENCES dbo.Person (person_id)
 );
 
@@ -108,9 +108,9 @@ CREATE TABLE dbo.Object2Keywords (
        object_id INTEGER NOT NULL
      , keyword_id INTEGER NOT NULL
      , PRIMARY KEY (object_id, keyword_id)
-     , FK_Object2Keywords_1 FOREIGN KEY (object_id)
+     , FOREIGN KEY (object_id)
                   REFERENCES dbo.Object (object_id)
-     , FK_Object2Keywords_2 FOREIGN KEY (keyword_id)
+     , FOREIGN KEY (keyword_id)
                   REFERENCES dbo.Keywords (keyword_id)
 );
 
@@ -118,9 +118,9 @@ CREATE TABLE dbo.DCC_Classification (
        object_id INTEGER NOT NULL
      , DCC_Categorie CHAR(10) NOT NULL
      , PRIMARY KEY (object_id, DCC_Categorie)
-     , FK_DCC_Classification_1 FOREIGN KEY (DCC_Categorie)
+     , FOREIGN KEY (DCC_Categorie)
                   REFERENCES dbo.DCC_Categories (DCC_Categorie)
-     , FK_DCC_Classification_2 FOREIGN KEY (object_id)
+     , FOREIGN KEY (object_id)
                   REFERENCES dbo.Object (object_id)
 );
 
@@ -128,9 +128,9 @@ CREATE TABLE dbo.DNB_Classification (
        object_id INTEGER NOT NULL
      , DNB_Categorie INTEGER NOT NULL
      , PRIMARY KEY (object_id, DNB_Categorie)
-     , FK_DNB_Classification_1 FOREIGN KEY (DNB_Categorie)
+     , FOREIGN KEY (DNB_Categorie)
                   REFERENCES dbo.DNB_Categories (DNB_Categorie)
-     , FK_DNB_Classification_2 FOREIGN KEY (object_id)
+     , FOREIGN KEY (object_id)
                   REFERENCES dbo.Object (object_id)
 );
 
@@ -138,9 +138,9 @@ CREATE TABLE dbo.DINI_Set_Classification (
        object_id INTEGER NOT NULL
      , DINI_set_id INTEGER NOT NULL
      , PRIMARY KEY (object_id, DINI_set_id)
-     , FK_DINI_Set_Classification_1 FOREIGN KEY (DINI_set_id)
+     , FOREIGN KEY (DINI_set_id)
                   REFERENCES dbo.DINI_Set_Categories (DINI_set_id)
-     , FK_DINI_Set_Classification_2 FOREIGN KEY (object_id)
+     , FOREIGN KEY (object_id)
                   REFERENCES dbo.Object (object_id)
 );
 
@@ -148,28 +148,28 @@ CREATE TABLE dbo.Other_Classification (
        object_id INTEGER NOT NULL
      , other_id INTEGER NOT NULL
      , PRIMARY KEY (object_id, other_id)
-     , FK_Other_Classification_1 FOREIGN KEY (other_id)
+     , FOREIGN KEY (other_id)
                   REFERENCES dbo.Other_Categories (other_id)
-     , FK_Other_Classification_2 FOREIGN KEY (object_id)
+     , FOREIGN KEY (object_id)
                   REFERENCES dbo.Object (object_id)
 );
 
 CREATE TABLE dbo.DateValues (
        object_id INTEGER NOT NULL
      , number INTEGER NOT NULL
-     , value TIMESTAMP
+     , value datetime
      , PRIMARY KEY (object_id, number)
-     , FK_DateValues_1 FOREIGN KEY (object_id)
+     , FOREIGN KEY (object_id)
                   REFERENCES dbo.Object (object_id)
 );
 
 CREATE TABLE dbo.Description (
        object_id INTEGER NOT NULL
      , number INTEGER NOT NULL
-     , abstract BLOB
+     , abstract TEXT
      , lang VARCHAR(3)
      , PRIMARY KEY (object_id, number)
-     , FK_Description_1 FOREIGN KEY (object_id)
+     , FOREIGN KEY (object_id)
                   REFERENCES dbo.Object (object_id)
 );
 
@@ -178,16 +178,16 @@ CREATE TABLE dbo.Publisher (
      , number INTEGER NOT NULL
      , name VARCHAR(255) NOT NULL
      , PRIMARY KEY (object_id, number)
-     , FK_Publisher_1 FOREIGN KEY (object_id)
+     , FOREIGN KEY (object_id)
                   REFERENCES dbo.Object (object_id)
 );
 
 CREATE TABLE dbo.Format (
        object_id INTEGER NOT NULL
      , number INTEGER NOT NULL
-     , schema VARCHAR(255) NOT NULL
+     , schema_f VARCHAR(255) NOT NULL
      , PRIMARY KEY (object_id, number)
-     , FK_Format_1 FOREIGN KEY (object_id)
+     , FOREIGN KEY (object_id)
                   REFERENCES dbo.Object (object_id)
 );
 
@@ -196,16 +196,15 @@ CREATE TABLE dbo.Identifier (
      , number INTEGER NOT NULL
      , identifier VARCHAR(255) NOT NULL
      , PRIMARY KEY (object_id, number)
-     , FK_Identifier_1 FOREIGN KEY (object_id)
+     , FOREIGN KEY (object_id)
                   REFERENCES dbo.Object (object_id)
 );
 
 CREATE TABLE dbo.RawData (
        object_id INTEGER NOT NULL
-     , collected TIMESTAMP NOT NULL
+     , collected datetime NOT NULL
      , data CHAR
      , PRIMARY KEY (object_id, collected)
-     , FK_RawData_1 FOREIGN KEY (object_id)
+     , FOREIGN KEY (object_id)
                   REFERENCES dbo.Object (object_id)
 );
-
