22b79d76a33e3006c91c0be7dc1c26f7ff91008c
klattman <klattman@fde32c5b-6e3e-0410-ac5f-c52931e6f8ac>
2008-02-07 09:33:03 +0000

    Die Abfrage fuer die Workflow-DB ist jetzt fertig. Steht unter "workflow-abfragen.sql"

--- develop/db-schema/workflow-abfragen.sql
+++ develop/db-schema/workflow-abfragen.sql
@@ -12,5 +12,13 @@ SELECT w1.workflow_id, w1.object_id, w1.time FROM dbo.WorkflowDB w1 WHERE w1.ser
 
 
 # Abfrage der zu bearbeitenden Service-ID
+SELECT w1.workflow_id, w1.service_id, so.predecessor_id FROM dbo.WorkflowDB w1 JOIN dbo.ServicesOrder so ON w1.service_id = so.predecessor_id and so.service_id = 2
 
-SELECT w1.workflow_id, w1.service_id, so1.predecessor_id FROM dbo.WorkflowDB w1 JOIN dbo.ServicesOrder so1 ON w1.service_id = so1.predecessor_id and so1.service_id = 2
+# vollstaendige Abfrage, evtl. muss noch optimiert werden, damit die Subqueries nicht immer fuer jeden 
+# Datensatz erfolgen muessen (und jeweils doppelt aufgrund des OR in der WHERE-Klausel;
+# die einzige effektive Einschraenkung ist immer die Service_id;
+# alternativ sollte man die Abfrage eventuell so gestalten, dass immer das ältestes Object zurückgegeben wird, ist eventuell billiger, da die Menge an Daten so kleiner wird
+
+SELECT w1.object_id FROM dbo.WorkflowDB w1 JOIN dbo.ServicesOrder so ON w1.service_id = so.predecessor_id and so.service_id = 2 
+where (w1.time > (select MAX(time) from dbo.WorkflowDB where object_id = w1.object_id and service_id= so.service_id)
+or w1.object_id not in (select object_id from dbo.WorkflowDB where object_id = w1.object_id and service_id = so.service_id)) group by object_id
