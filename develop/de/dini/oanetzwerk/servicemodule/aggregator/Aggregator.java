99129db8a6a645ceda21f36bf41218ed1f67f19b
klattman <klattman@fde32c5b-6e3e-0410-ac5f-c52931e6f8ac>
2008-06-10 12:44:02 +0000

    mehr debug-Meldungen im Aggregator

--- develop/de/dini/oanetzwerk/servicemodule/aggregator/Aggregator.java
+++ develop/de/dini/oanetzwerk/servicemodule/aggregator/Aggregator.java
@@ -57,7 +57,7 @@ import de.dini.oanetzwerk.utils.imf.Title;
  */
 
 /**
- * @author klattman
+ * @author Manuel Klatt-Kafemann
  * 
  */
 public class Aggregator {
@@ -263,6 +263,12 @@ public class Aggregator {
 
 	}
 
+	/*
+	 * implements workflow for a single entry
+	 * 
+	 * 
+	 * 
+	 */
 	private void startSingleRecord(int id) {
 		// TODO Auto-generated method stub
 		this.currentRecordId = id;
@@ -280,7 +286,9 @@ public class Aggregator {
 			return;
 		}
 		// Auseinandernehmen der Rohdaten
-		System.out.println("Geladene Daten: " + data);
+		System.out.println("Geladene Rohdaten (noch Base64-codiert): " + data);
+		
+		
 		if (logger.isDebugEnabled()) {
 			logger.debug("retrieved data: " + data);
 		}
@@ -342,8 +350,10 @@ public class Aggregator {
 	private Object decodeBase64(Object data) {
 		// Daten müssen Base64-decodiert werden
 
-		System.out.println(new String(Base64.decodeBase64(((String) data)
+		System.out.println("\n## Decodiert: \n\n" + new String(Base64.decodeBase64(((String) data)
 				.getBytes())));
+		System.out.println("## Ende decodierte Informationen\n\n\n");
+		
 		try {
 			return new String(Base64.decodeBase64(((String) data).getBytes()));
 		} catch (Exception e) {
@@ -575,6 +585,10 @@ public class Aggregator {
 
 		// return listEntrySet;
 
+		if (im != null) {
+			System.out.println("## InternalMetadata after Extraction: \n\n" + im.toString());
+		}
+		
 		return im;
 
 		// return null;
@@ -588,7 +602,7 @@ public class Aggregator {
 	}
 
 	private Object checkXML(Object data) {
-		// TODO Auto-generated method stub
+		// TODO checkXML not implemented
 		String result = (String) data;
 		return result;
 	}
@@ -639,9 +653,7 @@ public class Aggregator {
 		String value = "";
 
 		while (it.hasNext()) {
-
 			key = it.next();
-
 			if (key.equalsIgnoreCase("data")) {
 				value = entrySet.getValue(key);
 				break;
@@ -677,444 +689,5 @@ public class Aggregator {
 		return i;
 	}
 
-	/**
-	 * @param url
-	 * @param fullharvest
-	 * @param updateFrom
-	 * @param repositoryId
-	 */
-	//	
-	// private void processIds (String url, boolean fullharvest, String
-	// updateFrom, int repositoryId ) {
-	//		
-	// HttpClient client = new HttpClient ( );
-	// GetMethod getmethod;
-	// String resumptionToken = null;
-	// Boolean resumptionSet = true;
-	//		
-	// if (!fullharvest) {
-	//			
-	// getmethod = new GetMethod (url +
-	// "?verb=ListIdentifiers&metadataPrefix=oai_dc&from=" + updateFrom);
-	//			
-	// if (logger.isDebugEnabled ( ))
-	// logger.debug ("Update Harvest: " + url + getmethod.getQueryString ( ));
-	//			
-	// } else {
-	//			
-	// getmethod = new GetMethod (url +
-	// "?verb=ListIdentifiers&metadataPrefix=oai_dc");
-	//			
-	// if (logger.isDebugEnabled ( ))
-	// logger.debug ("Full Harvest: " + url + getmethod.getQueryString ( ));
-	// }
-	//		
-	// while (resumptionSet) {
-	//		
-	// client.getParams ( ).setParameter ("http.protocol.content-charset",
-	// "UTF-8");
-	//			
-	// try {
-	//				
-	// int statuscode = client.executeMethod (getmethod);
-	//				
-	// logger.info ("ID-List HttpStatusCode: " + statuscode);
-	//				
-	// if (statuscode != HttpStatus.SC_OK) {
-	//					
-	// //TODO: proper implementation
-	// ;
-	// }
-	//				
-	// resumptionToken = extractIds (getmethod.getResponseBodyAsStream ( ),
-	// repositoryId);
-	//				
-	// if (resumptionToken != null) {
-	//					
-	// if (logger.isDebugEnabled ( ))
-	// logger.debug ("ResumptionToken found: " + resumptionToken);
-	// else;
-	//					
-	// if (resumptionToken.equals ("")) {
-	//						
-	// if (logger.isDebugEnabled ( ))
-	// logger.debug ("ResumptionToken empty, IdentifierList complete");
-	// else;
-	//						
-	// resumptionSet = false;
-	//						
-	// } else {
-	//						
-	// getmethod = new GetMethod (url + "?verb=ListIdentifiers&resumptionToken="
-	// + resumptionToken);
-	//						
-	// if (logger.isDebugEnabled ( )) {
-	//							
-	// logger.debug ("ResumptionToken: " + resumptionToken);
-	// logger.debug ("ResumptionQuery: " + url + getmethod.getQueryString ( ));
-	//							
-	// } else;
-	// }
-	//					
-	// } else {
-	//					
-	// if (logger.isDebugEnabled ( ))
-	// logger.debug ("no ResumptionToken found, IdentifierList complete");
-	//					
-	// else;
-	//					
-	// resumptionSet = false;
-	// }
-	//			
-	// } catch (HttpException ex) {
-	//				
-	// logger.error (ex.getLocalizedMessage ( ));
-	// ex.printStackTrace ( );
-	// this.errorretry++;
-	//				
-	// if (errorretry > 5)
-	// System.exit (5);
-	//				
-	// } catch (IOException ex) {
-	//				
-	// logger.error (ex.getLocalizedMessage ( ));
-	// ex.printStackTrace ( );
-	//				
-	// this.errorretry++;
-	//				
-	// if (errorretry > 5)
-	// System.exit (5);
-	// }
-	// }
-	// }
-	//
-	/**
-	 * @param responseBody
-	 * @param repositoryId
-	 * @return
-	 */
-
-	// private String extractIds (InputStream responseBody, int repositoryId) {
-	//		
-	// String resumptionToken = null;
-	//		
-	// try {
-	//			
-	// //TODO: xml-handling has to be rewritten!
-	//			
-	// DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance ( );
-	// DocumentBuilder builder = factory.newDocumentBuilder ( );
-	// Document document = builder.parse (responseBody);
-	//			
-	// NodeList idNodeList = document.getElementsByTagName ("identifier");
-	// NodeList datestampNodeList = document.getElementsByTagName ("datestamp");
-	//			
-	// if (this.ids == null)
-	// this.ids = new ArrayList <ObjectIdentifier> ( );
-	//			
-	// if (logger.isDebugEnabled ( )) {
-	//				
-	// logger.debug ("we have " + idNodeList.getLength ( ) + " Ids to extract");
-	// logger.debug ("we have " + datestampNodeList.getLength ( ) + " Datestamps
-	// to extract");
-	// }
-	//			
-	// for (int i = 0; i < idNodeList.getLength ( ); i++) {
-	//				
-	// int internalOID;
-	// String externalOID = idNodeList.item (i).getTextContent ( );
-	// String datestamp = datestampNodeList.item (i).getTextContent ( );
-	//				
-	// if (logger.isDebugEnabled ( )) {
-	//					
-	// logger.debug ("List Record No. " + recordno++ + " " + externalOID + " " +
-	// datestamp);
-	// }
-	//				
-	// internalOID = objectexists (repositoryId, externalOID);
-	//				
-	// if (internalOID > 0) {
-	//					
-	// // we found this object in the database
-	//					
-	// if (logger.isDebugEnabled ( ))
-	// logger.debug (externalOID + " is " + internalOID + " in our database");
-	//					
-	// ids.add (new ObjectIdentifier (externalOID, datestamp, internalOID));
-	//					
-	// } else if (internalOID == -1) {
-	//					
-	// // we found no object in the database
-	//					
-	// if (logger.isDebugEnabled ( ))
-	// logger.debug (externalOID + " is not in the database. so we have to
-	// create a new object");
-	//					
-	// ids.add (new ObjectIdentifier (externalOID, datestamp, -1));
-	//					
-	// } else {
-	//					
-	// //TODO: better error-handling
-	// logger.error ("Error occured!");
-	// }
-	// }
-	//			
-	// NodeList rsList = document.getElementsByTagName ("resumptionToken");
-	//			
-	// if (rsList.getLength ( ) > 0)
-	// resumptionToken = rsList.item (0).getTextContent ( );
-	//			
-	// rsList = null;
-	// factory = null;
-	// builder = null;
-	// document = null;
-	//			
-	// idNodeList = null;
-	// datestampNodeList = null;
-	//			
-	// } catch (ParserConfigurationException pacoex) {
-	//			
-	// logger.error (pacoex.getLocalizedMessage ( ));
-	// pacoex.printStackTrace ( );
-	//			
-	// } catch (SAXException sex) {
-	//			
-	// logger.error (sex.getLocalizedMessage ( ));
-	// sex.printStackTrace ( );
-	//			
-	// } catch (IOException ioex) {
-	//			
-	// logger.error (ioex.getLocalizedMessage ( ));
-	// ioex.printStackTrace ( );
-	//			
-	// }
-	//		
-	// return resumptionToken;
-	// }
-	/**
-	 * @param url
-	 * @param id
-	 */
-	//	
-	// private void processRecords (String url, int id) {
-	//		
-	// HttpClient client;
-	// GetMethod method;
-	//		
-	// //String url =
-	// "http://edoc.hu-berlin.de/OAI-2.0?verb=GetRecord&metadataPrefix=oai_dc&identifier=";
-	//		
-	// if (logger.isDebugEnabled ( ))
-	// logger.debug ("now we process " + this.ids.size ( ) + "Records");
-	//		
-	// for (int i = 0; i < this.ids.size ( ); i++) {
-	//			
-	// // zuerst schauen, ob datestamp gleich, wenn ja, harvested auf today
-	// setzen, ansonsten metadaten putten
-	//			
-	// if (logger.isDebugEnabled ( ))
-	// logger.debug ("Obect No. " + i + " is processed");
-	//			
-	// int internalOID = this.ids.get (i).getInternalOID ( );
-	//			
-	// if (internalOID == -1) {
-	//				
-	// // when internalOID == -1 than Object is not in the database and we have
-	// to create it
-	//				
-	// String ressource = "ObjectEntry/";
-	// RestClient restClient = RestClient.createRestClient
-	// (this.props.getProperty ("host"), ressource, this.props.getProperty
-	// ("username"), this.props.getProperty ("password"));
-	//				
-	// GregorianCalendar cal = new GregorianCalendar ( );
-	// cal.setTime (this.ids.get (i).getDatestamp ( ));
-	//				
-	// String datestamp = cal.get (Calendar.YEAR) + "-" + (cal.get
-	// (Calendar.MONTH) + 1) + "-" + cal.get (Calendar.DAY_OF_MONTH);
-	//				
-	// if (logger.isDebugEnabled ( ))
-	// logger.debug ("creating Object No. " + i + ": " + this.ids.get
-	// (i).getExternalOID ( ));
-	//				
-	// try {
-	//					
-	// //TODO: rewrite XML!!!
-	// String result = restClient.PutData
-	// ("<oanrest>\n\t<request>\n\t\t<entryset>\n\t\t\t<entry
-	// key=\"repository_id\">" +
-	// id + "</entry>\n\t\t\t<entry key=\"repository_identifier\">" +
-	// this.ids.get (i).getExternalOID ( ) + "</entry>\n\t\t\t<entry
-	// key=\"repository_datestamp\">" +
-	// datestamp + "</entry>\n\t\t</entryset>\n\t</request>\n</oanrest>");
-	//					
-	// DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance ( );
-	// DocumentBuilder builder = factory.newDocumentBuilder ( );
-	// Document document = builder.parse (new InputSource (new StringReader
-	// (result)));
-	//					
-	// NodeList internalOIDList = document.getElementsByTagName ("OID");
-	//					
-	// if (logger.isDebugEnabled ( ))
-	// logger.debug ("resultlength = " + internalOIDList.getLength ( ));
-	//					
-	// int intoid = new Integer (internalOIDList.item (0).getTextContent ( ));
-	//					
-	// if (logger.isDebugEnabled ( ))
-	// logger.debug ("internalOID: " + intoid);
-	//					
-	// //tempobjid.setInternalOID (intoid);
-	//					
-	// this.ids.get (i).setInternalOID (intoid);
-	//					
-	// //this.ids.set (i, tempobjid);
-	//					
-	// } catch (SAXException saex) {
-	//					
-	// logger.error (saex.getLocalizedMessage ( ));
-	// saex.printStackTrace ( );
-	//					
-	// } catch (ParserConfigurationException ex) {
-	//					
-	// logger.error (ex.getLocalizedMessage ( ));
-	// ex.printStackTrace ( );
-	//					
-	// } catch (IOException ex) {
-	//					
-	// logger.error (ex.getLocalizedMessage ( ));
-	// ex.printStackTrace ( );
-	// }
-	//				
-	// } else {
-	//				
-	// // Object exists and we have to look if our Rawdata is newer than the
-	// database one
-	//				
-	// if (logger.isDebugEnabled ( ))
-	// logger.debug ("observing Object No. " + i + ": " + this.ids.get
-	// (i).getExternalOID ( ));
-	//				
-	// String ressource = "ObjectEntry/" + internalOID + "/";
-	// RestClient restclient = RestClient.createRestClient
-	// (this.props.getProperty ("host"), ressource, this.props.getProperty
-	// ("username"), this.props.getProperty ("password"));
-	//				
-	// String result = restclient.GetData ( );
-	//				
-	// try {
-	//					
-	// //TODO: rewrite XML
-	//					
-	// DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance ( );
-	// DocumentBuilder builder = factory.newDocumentBuilder ( );
-	// Document document = builder.parse (new InputSource (new
-	// StringReader(result)));
-	//					
-	// NodeList idDatestampList = document.getElementsByTagName
-	// ("repository_datestamp");
-	//					
-	// Date repositoryDate = new SimpleDateFormat ("yyyy-MM-dd").parse
-	// (idDatestampList.item (1).getTextContent ( ));
-	//					
-	// if (repositoryDate.before (this.ids.get (i).getDatestamp ( )) ||
-	// repositoryDate.equals (this.ids.get (i).getDatestamp ( ))) {
-	//						
-	// if (logger.isDebugEnabled ( )) {
-	//							
-	// logger.debug ("RepositoryDate is " + repositoryDate + " and before or
-	// equal the harvested: " + this.ids.get (i).getDatestamp ( ));
-	// }
-	// //TODO: implement proper change of harvested-timestamp
-	//						
-	// continue;
-	//						
-	// }
-	// } catch (SAXException saex) {
-	//					
-	// saex.printStackTrace ( );
-	//					
-	// } catch (ParserConfigurationException ex) {
-	//					
-	// ex.printStackTrace ( );
-	//					
-	// } catch (IOException ex) {
-	//					
-	// ex.printStackTrace ( );
-	//					
-	// } catch (DOMException ex) {
-	//					
-	// ex.printStackTrace ( );
-	//					
-	// } catch (java.text.ParseException ex) {
-	//					
-	// ex.printStackTrace ( );
-	// }
-	// }
-	//			
-	// client = new HttpClient ( );
-	// method = new GetMethod (url +
-	// "?verb=GetRecord&metadataPrefix=oai_dc&identifier=" + ids.get
-	// (i).getExternalOID ( ));
-	// client.getParams ( ).setParameter ("http.protocol.content-charset",
-	// "UTF-8");
-	//			
-	// try {
-	//				
-	// int statuscode = client.executeMethod (method);
-	//				
-	// logger.info ("HttpStatusCode: " + statuscode);
-	//				
-	// if (statuscode != HttpStatus.SC_OK) {
-	//					
-	// ;
-	// }
-	//				
-	// uploadRawData (HelperMethods.stream2String
-	// (method.getResponseBodyAsStream ( )), ids.get (i).getInternalOID ( ),
-	// ids.get (i).getDatestamp ( ));
-	//				
-	// } catch (HttpException ex) {
-	//				
-	// ex.printStackTrace ( );
-	//				
-	// } catch (IOException ex) {
-	//				
-	// ex.printStackTrace ( );
-	//								
-	// } finally {
-	//				
-	// method.releaseConnection ( );
-	// }
-	//			
-	// client = null;
-	// }
-	// }
-	//
-	/**
-	 * @param data
-	 * @param internalOID
-	 * @param datestamp
-	 * @throws IOException
-	 * @throws HttpException
-	 */
-
-	/*private void uploadRawData(String data, int internalOID, Date datestamp)
-			throws HttpException, IOException {
-
-		GregorianCalendar cal = new GregorianCalendar();
-		cal.setTime(datestamp);
-
-		String resource = "RawRecordData/" + internalOID + "/"
-				+ cal.get(Calendar.YEAR) + "-" + cal.get(Calendar.MONTH + 1)
-				+ "-" + cal.get(Calendar.DAY_OF_MONTH) + "/";
-
-		RestClient restclient = RestClient.createRestClient(this.props
-				.getProperty("host"), resource, this.props
-				.getProperty("username"), this.props.getProperty("password"));
-		restclient.PutData(data);
-
-		if (logger.isDebugEnabled())
-			logger.debug("uploaded rawdata for Database Object " + internalOID);
-	}*/
-
+	
 }
