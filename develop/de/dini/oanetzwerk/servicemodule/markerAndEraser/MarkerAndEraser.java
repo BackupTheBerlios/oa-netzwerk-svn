3316302e5c575c3203eda509514cb00c3630df45
kuehnmic <kuehnmic@fde32c5b-6e3e-0410-ac5f-c52931e6f8ac>
2008-06-24 15:06:34 +0000

    ADD added new ServiceModule for marking old data and erasing data marked as test

---
+++ develop/de/dini/oanetzwerk/servicemodule/markerAndEraser/MarkerAndEraser.java
@@ -0,0 +1,41 @@
+package de.dini.oanetzwerk.servicemodule.markerAndEraser;
+
+import java.math.BigDecimal;
+
+import org.apache.log4j.Logger;
+
+public class MarkerAndEraser {
+	
+	static Logger logger = Logger.getLogger (MarkerAndEraser.class); 
+	
+	public MarkerAndEraser ( ) {
+		
+		
+	}
+	
+	public void deleteTestData ( ) {
+		
+		//GetAllDataMarkedAsTest + Delete All where ObjectID in Marked as Test
+		
+		
+	}
+	
+	public void deleteTestData (BigDecimal repositoryID) {
+		
+		
+	}
+	
+	public void deleteTestData (String repositoryName) {
+		
+		
+	}
+	
+	public void notifyData ( ) {
+		
+	}
+	
+	public void outdated ( ) {
+		
+		
+	}
+}
---
+++ develop/de/dini/oanetzwerk/servicemodule/markerAndEraser/RunMarkerAndEraser.java
@@ -0,0 +1,12 @@
+package de.dini.oanetzwerk.servicemodule.markerAndEraser;
+
+public class RunMarkerAndEraser {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		new MarkerAndEraser ( );
+	}
+}
