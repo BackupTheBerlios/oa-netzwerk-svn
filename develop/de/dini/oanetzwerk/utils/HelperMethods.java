58b631059f44bdb46a635b6217bbd74c12418b5c
kuehnmic <kuehnmic@fde32c5b-6e3e-0410-ac5f-c52931e6f8ac>
2008-06-26 11:20:12 +0000

    UPDATE added new method concerning converting cmdlinevalues into BigDecimals

--- develop/de/dini/oanetzwerk/utils/HelperMethods.java
+++ develop/de/dini/oanetzwerk/utils/HelperMethods.java
@@ -11,6 +11,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 import java.sql.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -143,5 +144,13 @@ public class HelperMethods {
 		sdf = null;
 		
 		return date;
+	}
+	
+
+	public static BigDecimal getBigDecimalFromCmdLine (String optionValue) throws NumberFormatException {
+		
+		BigDecimal result = new BigDecimal (optionValue);
+		
+		return result;
 	}
 }
