ea45829c51b3473e5f0c597d2d866c054a245d0f
klattman <klattman@fde32c5b-6e3e-0410-ac5f-c52931e6f8ac>
2008-08-19 12:00:32 +0000

    Änderung DB-Schema:
    - neue Tabelle "LoginData" für Passwörter

--- develop/db-schema/oan-db-model2.sql
+++ develop/db-schema/oan-db-model2.sql
@@ -1,3 +1,9 @@
+CREATE TABLE dbo.LoginData (
+	name UNIVARCHAR(50) NOT NULL UNIQUE,
+	email UNIVARCHAR(100) NOT NULL,
+	password UNIVARCHAR(255) NOT NULL
+)
+
 CREATE TABLE dbo.ServiceNotify (
        service_id NUMERIC(38) NOT NULL
 	 , inserttime DATETIME NOT NULL
