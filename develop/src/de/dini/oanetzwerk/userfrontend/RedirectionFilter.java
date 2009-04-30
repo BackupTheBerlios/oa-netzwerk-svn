86619b36aa655489fe3ed59522a1b0828b77885a
malitzro <malitzro@fde32c5b-6e3e-0410-ac5f-c52931e6f8ac>
2009-04-30 14:39:15 +0000

    redirectfilter fuer userfrontend (aenderungen an web.xml nicht eingecheckt)

---
+++ develop/src/de/dini/oanetzwerk/userfrontend/RedirectionFilter.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package de.dini.oanetzwerk.userfrontend;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Michael K&uuml;hn
+ *
+ */
+
+public class RedirectionFilter implements Filter {
+	
+	public final static String EXT = "faces";
+	
+	/**
+	 * @see javax.servlet.Filter#destroy()
+	 */
+	
+	public void destroy ( ) { }
+	
+	/**
+	 * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest, javax.servlet.ServletResponse, javax.servlet.FilterChain)
+	 */
+	
+	public void doFilter (ServletRequest servletrequest, ServletResponse servletresponse, FilterChain chain) throws IOException, ServletException {
+		
+		HttpServletRequest httpservreq = (HttpServletRequest) servletrequest;
+		HttpServletResponse httpservres = (HttpServletResponse) servletresponse;
+		String uri = httpservreq.getRequestURI ( );
+		
+		if (uri.endsWith ("*.jsp")) {
+			
+			int length = uri.length ( );
+			
+			String newuri = uri.substring (0, length - 3) + EXT;
+			
+			httpservres.sendRedirect (newuri);
+			
+		} else if(uri.endsWith ("*."+EXT)) {
+			// do nothing
+			chain.doFilter(servletrequest, servletresponse);
+		} else {
+			httpservres.sendRedirect ("/findnbrowse/index.faces");
+		}
+	}
+	
+	/**
+	 * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
+	 */
+	
+	public void init (FilterConfig arg0) throws ServletException { }
+	
+}
