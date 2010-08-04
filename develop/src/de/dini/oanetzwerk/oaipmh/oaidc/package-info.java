51a3fcac51766d4f60f3e28d8e88fb8333dbccf2
sdavid <sdavid@fde32c5b-6e3e-0410-ac5f-c52931e6f8ac>
2010-08-04 14:06:38 +0000

    remove in order to freshly check in new version

--- develop/src/de/dini/oanetzwerk/oaipmh/oaidc/package-info.java
+++
@@ -1,5 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(xmlns = {
-        @javax.xml.bind.annotation.XmlNs(prefix = "oai_dc", namespaceURI="http://www.openarchives.org/OAI/2.0/oai_dc/"),
-        @javax.xml.bind.annotation.XmlNs(prefix = "dc", namespaceURI="http://purl.org/dc/elements/1.1/")
-        })
-package de.dini.oanetzwerk.oaipmh.oaidc;
