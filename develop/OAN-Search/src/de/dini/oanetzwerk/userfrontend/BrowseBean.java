1865d7e82bb6d9fd8dc29e08a90ef0771e6040ae
sdavid <sdavid@fde32c5b-6e3e-0410-ac5f-c52931e6f8ac>
2011-01-11 16:50:43 +0000

    oan-search - 3rd level navigation bug fix

--- develop/OAN-Search/src/de/dini/oanetzwerk/userfrontend/BrowseBean.java
+++ develop/OAN-Search/src/de/dini/oanetzwerk/userfrontend/BrowseBean.java
@@ -159,240 +159,24 @@ public class BrowseBean {
 			return cat1 - cat2;
 		}
 		
-	}
-	
-//	private List<String[]> generateSimpleDDCCategorySums() {
-//			
-//		ArrayList<String[]> listDDCCategoriesAndSum = new ArrayList<String[]>();
-//		RestMessage rms = this.prepareRestTransmission ("DDCCategories/").sendGetRestMessage();
-//		
-//		if (rms == null || rms.getListEntrySets ( ).isEmpty ( )) {
-//			
-//			logger.error ("received no DDCCategories");
-//			return null;
-//		}
-//		
-//		if (rms.getStatus() != RestStatusEnum.OK) {
-//			
-//			logger.error ("received status " + rms.getStatus() + " (" + rms.getStatusDescription() + ") for DDCCategories");
-//			return null;
-//		}
-//		
-//		for (RestEntrySet res : rms.getListEntrySets ( )) {
-//			
-//			Iterator <String> it = res.getKeyIterator ( );
-//			String key = "";
-//			
-//			while (it.hasNext ( )) {
-//				key = it.next ( );
-//				listDDCCategoriesAndSum.add(new String[] {key, res.getValue (key)} );	
-//				mapDDCSums.put(key, res.getValue (key));
-//			}
-//			
-//		}
-//		
-//		Collections.sort(listDDCCategoriesAndSum, new CategoryNumberComparator());
-//		
-//		return listDDCCategoriesAndSum;
-//	}
-
-//	private List<String[]> generateDirectDDCCategorySums(List<String[]> listSimpleSums) {
-//		
-//		ArrayList<String[]> listDDCCategoriesAndSum = new ArrayList<String[]>();
-//		
-//		for(String entry[] : listSimpleSums) {
-//			
-//			String wildcardCategory = entry[0];
-//			wildcardCategory = wildcardCategory.replaceAll("00$", "x");
-//			wildcardCategory = wildcardCategory.replaceAll("0$", "x");
-//			
-//			RestMessage rms = this.prepareRestTransmission ("DDCCategories/" + wildcardCategory).sendGetRestMessage();
-//
-//			if (rms == null || rms.getListEntrySets ( ).isEmpty ( )) {
-//				logger.error ("received no DDCCategories");
-//				return null;
-//			}
-//
-//			if (rms.getStatus() != RestStatusEnum.OK) {
-//				logger.error ("received status " + rms.getStatus() + " (" + rms.getStatusDescription() + ") for DDCCategories");
-//				return null;
-//			}
-//
-//			for (RestEntrySet res : rms.getListEntrySets ( )) {
-//
-//				Iterator <String> it = res.getKeyIterator ( );
-//				String key = "";
-//
-//				while (it.hasNext ( )) {
-//					key = it.next ( );
-//					if(key.equals("sum")) listDDCCategoriesAndSum.add(new String[] {entry[0], res.getValue (key), wildcardCategory} );				
-//				}
-//
-//			}
-//		}
-//				
-//		return listDDCCategoriesAndSum;
-//	}
-//
-//	private DDCNaviNode getDDCNaviNodeForCat(String strCategory) {
-//		DDCNaviNode node = new DDCNaviNode();
-//		
-//		String strWildcardCategory = strCategory;
-//		strWildcardCategory.replaceAll("00$", "x");
-//		strWildcardCategory.replaceAll("0$", "x");
-//		strWildcardCategory.replaceAll("[.][0-9]", ".x");
-//		
-//		String sum = "";
-//					
-//		RestMessage rms = this.prepareRestTransmission ("DDCCategories/" + strWildcardCategory).sendGetRestMessage();
-//
-//		if (rms == null || rms.getListEntrySets ( ).isEmpty ( )) {
-//			logger.error ("received no DDCCategories");
-//			return null;
-//		}
-//
-//		if (rms.getStatus() != RestStatusEnum.OK) {
-//			logger.error ("received status " + rms.getStatus() + " (" + rms.getStatusDescription() + ") for DDCCategories");
-//			return null;
-//		}
-//
-//		for (RestEntrySet res : rms.getListEntrySets ( )) {
-//			Iterator <String> it = res.getKeyIterator ( );
-//			String key = "";
-//			while (it.hasNext ( )) {
-//				key = it.next ( );					
-//				if(key.equals("sum")) sum = res.getValue(key);
-//			}												
-//		}
-//		
-//		node.setStrDDCValue(strCategory);
-//		String name = (String)mapDDCNames_de.get(strCategory);
-//		node.setStrNameDE(name);
-//		try {
-//			node.setLongItemCount(Long.parseLong(sum));
-//		} catch (Exception ex) {
-//			node.setLongItemCount(-1);
-//		}
-//		
-//		return node;
-//	}
-//	
-//	private List<DDCNaviNode> generateListDDCNaviNodes() {
-//		
-//		List<DDCNaviNode> newList = new ArrayList<DDCNaviNode>();
-//		
-//		for(int i = 0; i < 10; i++) {			
-//			
-//			String strCategory_lvl1 = "" + i + "00";
-//			DDCNaviNode node_lvl1 = getDDCNaviNodeForCat(strCategory_lvl1);
-//			
-//			for(int j = 1; j < 10; j++) {
-//				
-//				String strCategory_lvl2 = "" + i + j + "0";
-//				DDCNaviNode node_lvl2 = getDDCNaviNodeForCat(strCategory_lvl2);
-//				
-//				for(int k = 1; k < 10; k++) {
-//					
-//					String strCategory_lvl3 = "" + i + j + k;
-//					DDCNaviNode node_lvl3 = getDDCNaviNodeForCat(strCategory_lvl3);
-//					
-//					/*
-//					for(int l = 1; l < 10; l++) {
-//						
-//						String strCategory_lvl4 = "" + i + j + k + "." + l;
-//						DDCNaviNode node_lvl4 = getDDCNaviNodeForCat(strCategory_lvl4);
-//												
-//						if(node_lvl4.getLongItemCount() > 0) node_lvl3.getListSubnodes().add(node_lvl4);
-//						
-//					}*/
-//					
-//					if(node_lvl3.getLongItemCount() > 0) node_lvl2.getListSubnodes().add(node_lvl3);
-//					
-//				}
-//				
-//				if(node_lvl2.getLongItemCount() > 0) node_lvl1.getListSubnodes().add(node_lvl2);
-//			}
-//			
-//			if(node_lvl1.getLongItemCount() > 0) newList.add(node_lvl1);
-//		}
-//		
-//		return newList;
-//	}
-//	
-//	private List<DDCNaviNode> generateListDDCNaviNodes(List<String[]> flatList) {
-//		
-//		List<DDCNaviNode> nodeList = new ArrayList<DDCNaviNode>();
-//		/*		
-//		for(String[] entry : flatList) {
-//			Pattern p = Pattern.compile( "[0-9]00");		
-//			Matcher m = p.matcher(entry[0]); 
-//			if(m.matches()) {
-//				DDCNaviNode node = new DDCNaviNode();
-//				node.setStrDDCValue(entry[0]);
-//				try {
-//					node.setLongItemCount(Long.parseLong(entry[1]));
-//				} catch (Exception ex) {
-//					node.setLongItemCount(-1);
-//				}
-//				String name = (String)mapDDCNames_de.get(entry[0]);
-//				node.setStrNameDE(name);
-//				nodeList.add(node);
-//			}
-//		}	*/
-//		
-//		for(int i = 0; i < 10; i++) {
-//			DDCNaviNode node1 = new DDCNaviNode();
-//			String cat1 = ""+i+"00";
-//			node1.setStrDDCValue(cat1);
-//			try {
-//				String sum = mapDDCSums.get(cat1);
-//				if(sum == null) sum = "0";
-//				node1.setLongItemCount(Long.parseLong(sum));
-//			} catch (Exception ex) {
-//				node1.setLongItemCount(-1);
-//			}
-//			String name1 = (String)mapDDCNames_de.get(cat1);
-//			node1.setStrNameDE(name1);
-//			for(int j = 1; j < 10; j++) {
-//				DDCNaviNode node2 = new DDCNaviNode();
-//				String cat2 = ""+i+j+"0";
-//				node2.setStrDDCValue(cat2);
-//				try {
-//					String sum = mapDDCSums.get(cat2);
-//					if(sum == null) sum = "0";
-//					node2.setLongItemCount(Long.parseLong(sum));
-//				} catch (Exception ex) {
-//					node2.setLongItemCount(-1);
-//				}		
-//				String name2 = (String)mapDDCNames_de.get(cat2);
-//				node2.setStrNameDE(name2);
-//				if(node2.getLongItemCount() > 0) node1.getListSubnodes().add(node2);
-//			}
-//			if(node1.getLongItemCount() > 0) nodeList.add(node1);			
-//		}		
-//		
-//		return nodeList;
-//	}
-	
+	}	
 	
 	public void addDDCCategoryToPath(String strDDCValue) {
 		int level = 1;
 		int ddc = 0; try {ddc = Integer.parseInt(strDDCValue.substring(0, 3));} catch(Exception ex) {}
 		if(ddc % 100 == 0) level = 0;
-		if(ddc % 10 != 0 && ddc % 100 > 9) level = 2;
-
+		if(ddc % 10 != 0 && ddc % 100 != 0) level = 2;
 		this.pathDDCCategories = new ArrayList<String>();
 		if(level == 0) {
 			this.pathDDCCategories.add(strDDCValue);
 		} else if(level == 1) {
-			this.pathDDCCategories.add(strDDCValue.charAt(0)+"00");
+			this.pathDDCCategories.add(strDDCValue.charAt(0)+ "00");
 			this.pathDDCCategories.add(strDDCValue);
 		} else if(level == 2) {
 			this.pathDDCCategories.add(strDDCValue.charAt(0)+"00");
-			this.pathDDCCategories.add(strDDCValue.charAt(0)+strDDCValue.charAt(1)+"0");
+			this.pathDDCCategories.add(new StringBuffer().append(strDDCValue.charAt(0)).append(strDDCValue.charAt(1)).append('0').toString());
 			this.pathDDCCategories.add(strDDCValue);
-		}
-		
+		} 
 	}
 		
 	public String actionUnselectDDCCategoryLink() {
@@ -401,4 +185,6 @@ public class BrowseBean {
 		this.setSelectedDDCCatName("Alles");
 		return "ddc_category_selected";
 	}
+	
+	
 }
