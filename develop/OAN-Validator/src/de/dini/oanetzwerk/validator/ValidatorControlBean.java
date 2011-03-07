9761c8bd4e44fa7ea31ec5cbaef0632e48d90c76
sdavid <sdavid@fde32c5b-6e3e-0410-ac5f-c52931e6f8ac>
2011-03-07 16:01:23 +0000

    OAN-Validator - libs changed - some refactoring

--- develop/OAN-Validator/src/de/dini/oanetzwerk/validator/ValidatorControlBean.java
+++ develop/OAN-Validator/src/de/dini/oanetzwerk/validator/ValidatorControlBean.java
@@ -4,6 +4,8 @@ import gr.uoa.di.validator.api.IJob;
 import gr.uoa.di.validator.api.JobActionsAPI;
 import gr.uoa.di.validator.api.JobRunner;
 import gr.uoa.di.validator.api.SgParameters;
+import gr.uoa.di.validator.api.Validator;
+import gr.uoa.di.validator.api.standalone.APIStandalone;
 import gr.uoa.di.validator.constants.FieldNames;
 import gr.uoa.di.validator.jobs.JobListener;
 import gr.uoa.di.validator.standalone.APIFactory;
@@ -19,6 +21,14 @@ public class ValidatorControlBean implements JobListener {
 
 	
 	private String baseUrl;
+	private String numOfRecords;
+	private Boolean randomRecordSelection = false;
+	private String oaiContentSet;
+	private String jobType;
+	
+	// TODO: automatically detect logged in user
+	private String user = "sdavid";
+	
 	
 	
 	public ValidatorControlBean() {
@@ -28,39 +38,40 @@ public class ValidatorControlBean implements JobListener {
 	
 		System.out.println("Validation process about to start...");
 		System.out.println("Url to validate: " + baseUrl);
+		System.out.println("job type: " + jobType);
+		System.out.println("# records: " + numOfRecords);
+		System.out.println("random mode: " + randomRecordSelection);
+		System.out.println("selected set: "+ oaiContentSet);
 		
 		
-//		SgParameters params = new SgParameters();
-//		params.addParam(FieldNames.JOB_GENERAL_USER, "sdavid");
-//		params.addParam(FieldNames.JOB_GENERAL_TYPE, "OAI Usage Validation");
-//		params.addParam(FieldNames.JOB_OAIUSAGE_BASEURL, baseUrl);
+		
+		SgParameters params = new SgParameters();
+		
+		
+		params.addParam(FieldNames.JOB_GENERAL_USER, "sdavid");
+		params.addParam(FieldNames.JOB_GENERAL_TYPE, "OAI Usage Validation");
+		params.addParam(FieldNames.JOB_OAIUSAGE_BASEURL, baseUrl);
+		
+		// general job definition
+//		params.addParam(FieldNames.JOB_GENERAL_USER, "testuser");
+//		params.addParam(FieldNames.JOB_GENERAL_TYPE, "OAI Content Validation");
+//		params.addParam(FieldNames.JOB_OAICONTENT_BASEURL, baseUrl);
+//		params.addParam(FieldNames.JOB_OAICONTENT_RANDOM, "false");
+//		params.addParam(FieldNames.JOB_OAICONTENT_RECORDS, "200");
+//		params.addParam(FieldNames.JOB_OAICONTENT_SET, "driver");
 
-		SgParameters params = new SgParameters(); 
-		params.addParam(FieldNames.JOB_GENERAL_USER, "testuser");
-		params.addParam(FieldNames.JOB_GENERAL_TYPE, "OAI Content Validation");
-		params.addParam(FieldNames.JOB_OAICONTENT_BASEURL, baseUrl);
-		params.addParam(FieldNames.JOB_OAICONTENT_RANDOM, "false");
-		params.addParam(FieldNames.JOB_OAICONTENT_RECORDS, "200");
-		params.addParam(FieldNames.JOB_OAICONTENT_SET, "driver");
-
-		
-		
-		// Rule definition
-		
+		// rule definition		
 		List<Integer> ruleIds = new ArrayList<Integer>();
 		ruleIds.add(1);
 
-		JobActionsAPI jobActionsAPI = APIFactory.getJobActionsAPI();
 
 		try {
-	        IJob job1 = jobActionsAPI.addNewJob(params, ruleIds);
-	        
-	        job1.addListener(this);
-	        System.out.println(job1.toString());
-	        System.out.println("OUT: " + job1.getJob().checkParams(params));
-	        
-	        JobRunner jobRunner = new JobRunner();
-	        jobRunner.startJob(job1);
+			
+			Validator val = APIStandalone.getValidator();
+			
+			IJob job = val.addNewJob(params, ruleIds);
+			job.addListener(this);
+			val.startJob(job);
 
         } catch (Exception e) {
 	        e.printStackTrace();
